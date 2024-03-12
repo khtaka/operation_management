347bf5bbeda9b543850b3efad77af66e4446b196
khtaka <khtaka108@gmail.com>
2024-03-12 18:02:29 +0900

    LESSON2

---
+++ src/main/java/com/example/mapper/CarMapper.java
@@ -0,0 +1,25 @@
+package com.example.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.entity.Car;
+
+@Mapper
+public interface CarMapper {
+	public List<Car> findAll();
+	    
+	    
+	    public List<Car> findAll(@Param("carId") Integer carId, @Param("carName") String carName,@Param("carPassengers")Integer carPassengers);
+	 
+	    
+	    public void insert(Car car);
+	    
+	    public Car findById(Integer id);
+
+	    public void update(Car car);
+	    
+	    public void deleteById(Integer id);
+}
